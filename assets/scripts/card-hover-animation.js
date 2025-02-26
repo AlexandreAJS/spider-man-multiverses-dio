f433c40498d16047f46ac0dd6bf49f46a39e3540
AlexandreAJS <alexandreaj7santos@gmail.com>
2025-02-26 16:59:41 -0300

    Corrigida funcao mouseEnterCard

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -1,12 +1,15 @@
-function mouseOverCard(event) {
-    console.log('handle mouse enter called', this);
+function mouseEnterCard() {
+   this.classList.add('card--hover') //this se refere ao elemento que disparou o evento, neste caso o card
+    /*classList é uma propriedade do elemento DOM que retorna uma coleção DOMTokenList dos nomes das classes CSS
+    atribuídas ao elemento. Essa propriedade permite manipular as classes CSS de um elemento de forma conveniente.
+    */
 }
 
 function addEventListenersToCards(){
     const cardElements = document.getElementsByClassName('card');
     for (let index = 0; index < cardElements.length; index++) {
         const card = cardElements[index];
-        card.addEventListener('mouseenter', mouseOverCard());
+        card.addEventListener('mouseenter', mouseEnterCard);
     }
 }
 
