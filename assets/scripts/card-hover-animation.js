ee7a0fd421ae3ce2e850407d0fa803212968a4e9
AlexandreAJS <alexandreaj7santos@gmail.com>
2025-02-26 22:26:55 -0300

    Alteracoes nos efeitos do card

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -1,8 +1,14 @@
 function mouseEnterCard() {
-   this.classList.add('card--hover') //this se refere ao elemento que disparou o evento, neste caso o card
+   this.classList.add('card--hovered'); //this se refere ao elemento que disparou o evento, neste caso o card
     /*classList é uma propriedade do elemento DOM que retorna uma coleção DOMTokenList dos nomes das classes CSS
     atribuídas ao elemento. Essa propriedade permite manipular as classes CSS de um elemento de forma conveniente.
     */
+   document.body.id= (`${this.id}--hovered`);
+}
+
+function mouseLeaveCard() {
+    this.classList.remove('card--hovered');
+    document.body.id = '';
 }
 
 function addEventListenersToCards(){
@@ -10,6 +16,7 @@ function addEventListenersToCards(){
     for (let index = 0; index < cardElements.length; index++) {
         const card = cardElements[index];
         card.addEventListener('mouseenter', mouseEnterCard);
+        card.addEventListener('mouseleave', mouseLeaveCard);
     }
 }
 
